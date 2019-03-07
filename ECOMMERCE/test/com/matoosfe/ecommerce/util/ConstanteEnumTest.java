efe66fd57dc622b096f804ae4a01424e626207d0
martosfre <marcgigolito57@gmail.com>
2019-03-06 20:28:46 -0500

    Manejo de Constantes

---
+++ ECOMMERCE/test/com/matoosfe/ecommerce/util/ConstanteEnumTest.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.matoosfe.ecommerce.util;
+
+/**
+ * Tipo de clase para definir constantes con más de un valor.
+ * @author martosfre
+ * 6 mar. 2019 - 20:11:48
+ */
+public enum ConstanteEnumTest {
+	EMPRESA("Matoosfe", "1789564213501"),
+	COLOR_BLANCO("Blanco", "#ffffff"),
+	DIRECTORIO_GIT("ProyectoGIT", "C:\\CURSO_JAVAI\\ProyectosGit");
+	
+	private String nombre;
+	private String valor;
+	
+	private ConstanteEnumTest(String nombre, String valor) {
+		this.nombre = nombre;
+		this.valor = valor;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @return the valor
+	 */
+	public String getValor() {
+		return valor;
+	}
+	
+}
---
+++ ECOMMERCE/test/com/matoosfe/ecommerce/util/ConstanteTest.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.matoosfe.ecommerce.util;
+
+/**
+ * Clase para ejemplificar el uso de constantes
+ * @author martosfre
+ * 6 mar. 2019 - 19:59:22
+ */
+public class ConstanteTest {
+	/*
+	 * Para definir una constante se tiene las siguientes observaciones:
+	 * - Toda constante es public
+	 * - Para definir una constante se utiliza los modificadores de comportamiento
+	 *   static final
+	 * - Las constantes siempre van con mayúscula
+	 * - Para asociar constantes se puede utilizar sufijos, prefijos o crear clases con 
+	 *   varias constantes. Por ejemplo ConstanteEmpresa.java tendria las constantes de
+	 *   Empresa
+	 */
+	public static final String NOMBRE_EMPRESA = "Matoosfe";
+	public static final String RUC_EMPRESA = "1789564213501";
+	public final static String DIRECTORIO_GIT = "C:\\CURSO_JAVAI\\ProyectosGit";
+	public final static String COLOR_BLANCO = "Blanco";
+	public final static String COLOR_BLANCO_CODIGOEXA = "#ffffff";
+	
+	
+	
+	
+	
+	
+	
+}
